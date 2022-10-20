b3fd11a40e6b664f8bf611c0899a36fd2da2272e
jeff-dillon <dillonjg@gmail.com>
2022-10-20 14:04:36 -0400

    implemented basic functionality

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
-const gridContainer = document.querySelector(".grid-container");
-console.log(gridContainer);
-gridContainer.textContent = "Hello, World!";
-
 function toggleHighlight(e) {
-    console.log(e.srcElement);
-    if(e.srcElement.classList.contains('highlight')) {
-        e.srcElement.classList.remove('highlight');
-    } else {
-        e.srcElement.classList.add('highlight');
+    e.srcElement.classList.add('highlight');
+}
+
+function createGrid(numBoxes) {
+    const gridContainer = document.querySelector(".grid-container");
+    gridContainer.style.setProperty('--grid-rows', numBoxes);
+    gridContainer.style.setProperty('--grid-cols', numBoxes);
+    for(i = 0; i < numBoxes * numBoxes; i++) {
+        const div = document.createElement('div');
+        div.className = 'block';
+        div.addEventListener('mouseover', toggleHighlight);
+        gridContainer.appendChild(div);
     }
 }
 
-gridContainer.addEventListener('mouseover', toggleHighlight);
-gridContainer.addEventListener('mouseout', toggleHighlight);
+function clearGrid() {
+    const gridContainer = document.querySelector(".grid-container");
+    while(gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+}
+
+function promptForSize() {
+    let gridSize = 0;
+    do {
+        gridSize = parseInt(prompt("Enter grid size (1-100):"));
+    } while (isNaN(gridSize) || gridSize > 100 || gridSize < 1);
+    clearGrid();
+    createGrid(gridSize);
+}
+
+const generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener('click', promptForSize);
+
+const defaultGridSize = 16;
+createGrid(defaultGridSize);
